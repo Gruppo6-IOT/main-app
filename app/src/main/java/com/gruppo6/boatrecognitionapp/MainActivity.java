06d7a94bd1ccca34ce251214ce945d7eb0d79de6
cisc0f <francesco@coacci.it>
2022-04-28 16:48:53 +0200

    Camera implemented

--- app/src/main/java/com/gruppo6/boatrecognitionapp/MainActivity.java
+++ app/src/main/java/com/gruppo6/boatrecognitionapp/MainActivity.java
@@ -1,14 +1,57 @@
 package com.gruppo6.boatrecognitionapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String[] CAMERA_PERMISSION = new String[]{Manifest.permission.CAMERA};
+    private static final int CAMERA_REQUEST_CODE = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnStartCamera = findViewById(R.id.btn_start_camera);
+
+        btnStartCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (hasCameraPermission()) {
+                    startCamera();
+                } else {
+                    requestPermission();
+                }
+            }
+        });
+
+    }
+
+    private boolean hasCameraPermission() {
+        return ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(
+                this,
+                CAMERA_PERMISSION,
+                CAMERA_REQUEST_CODE
+        );
+    }
+
+    private void startCamera() {
+        Intent intent = new Intent(this, CameraActivity.class);
+        startActivity(intent);
     }
 }

7ebbed3099d2302afb73b0b22bc1949e5fe3e19b
cisc0f <francesco@coacci.it>
2022-06-15 15:47:59 +0200

    AI working

--- app/src/main/java/com/gruppo6/boatrecognitionapp/CameraActivity.java
+++ app/src/main/java/com/gruppo6/boatrecognitionapp/CameraActivity.java
@@ -2,8 +2,11 @@ package com.gruppo6.boatrecognitionapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
@@ -11,19 +14,51 @@ import androidx.camera.view.PreviewView;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 
+import android.annotation.SuppressLint;
+import android.graphics.Rect;
+import android.media.Image;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.util.Log;
 import android.util.Size;
 import android.view.OrientationEventListener;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.common.util.concurrent.ListenableFuture;
+import com.google.mlkit.common.model.LocalModel;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.objects.DetectedObject;
+import com.google.mlkit.vision.objects.ObjectDetection;
+import com.google.mlkit.vision.objects.ObjectDetector;
+import com.google.mlkit.vision.objects.custom.CustomObjectDetectorOptions;
+import com.google.mlkit.vision.objects.defaults.ObjectDetectorOptions;
+import com.google.mlkit.vision.objects.defaults.PredefinedCategory;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class CameraActivity extends AppCompatActivity {
 
     private PreviewView previewView;
-    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private Button captureImage;
+    private Executor executor = Executors.newSingleThreadExecutor();
+    LocalModel localModel =
+            new LocalModel.Builder()
+                    .setAssetFilePath("mobilenet.tflite")
+                    .build();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,36 +66,102 @@ public class CameraActivity extends AppCompatActivity {
         setContentView(R.layout.activity_camera);
 
         previewView = findViewById(R.id.preview_view);
-        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        captureImage = findViewById(R.id.captureImg);
+
+        final ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+
         cameraProviderFuture.addListener(new Runnable() {
             @Override
             public void run() {
                 try {
+
                     ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
-                    bindImageAnalysis(cameraProvider);
+                    bindPreview(cameraProvider);
+
                 } catch (ExecutionException | InterruptedException e) {
-                    e.printStackTrace();
+                    // No errors need to be handled for this Future.
+                    // This should never be reached.
                 }
             }
         }, ContextCompat.getMainExecutor(this));
     }
 
-    private void bindImageAnalysis(@NonNull ProcessCameraProvider cameraProvider) {
-        ImageAnalysis imageAnalysis =
-                new ImageAnalysis.Builder().setTargetResolution(new Size(1920, 1080))
-                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST).build();
-        imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(this), new ImageAnalysis.Analyzer() {
+    void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+
+        Preview preview = new Preview.Builder()
+                .build();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
+                .build();
+
+        ImageCapture.Builder builder = new ImageCapture.Builder();
+
+        final ImageCapture imageCapture = builder
+                .setTargetRotation(this.getWindowManager().getDefaultDisplay().getRotation())
+                .build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview, imageAnalysis, imageCapture);
+
+
+        CustomObjectDetectorOptions customObjectDetectorOptions =
+                new CustomObjectDetectorOptions.Builder(localModel)
+                        .setDetectorMode(CustomObjectDetectorOptions.SINGLE_IMAGE_MODE)
+                        .enableMultipleObjects()
+                        .enableClassification()
+                        .setClassificationConfidenceThreshold(0.5f)
+                        .setMaxPerObjectLabelCount(3)
+                        .build();
+
+        ObjectDetector objectDetector =
+                ObjectDetection.getClient(customObjectDetectorOptions);
+
+        captureImage.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void analyze(@NonNull ImageProxy image) {
-                image.close();
+            public void onClick(View v) {
+
+                imageCapture.takePicture(executor, new ImageCapture.OnImageCapturedCallback() {
+                    @Override
+                    public void onCaptureSuccess(@NonNull ImageProxy imageProxy) {
+                        super.onCaptureSuccess(imageProxy);
+                        @SuppressLint("UnsafeOptInUsageError") Image mediaImage = imageProxy.getImage();
+                        InputImage image =
+                                InputImage.fromMediaImage(mediaImage, imageProxy.getImageInfo().getRotationDegrees());
+                        objectDetector.process(image)
+                                .addOnSuccessListener(
+                                        new OnSuccessListener<List<DetectedObject>>() {
+                                            @Override
+                                            public void onSuccess(List<DetectedObject> results) {
+                                                // Task completed successfully
+                                                for (DetectedObject detectedObject : results) {
+                                                    Rect boundingBox = detectedObject.getBoundingBox();
+                                                    Integer trackingId = detectedObject.getTrackingId();
+                                                    for (DetectedObject.Label label : detectedObject.getLabels()) {
+                                                        String text = label.getText();
+                                                        Log.d("Object recognized", text);
+                                                        int index = label.getIndex();
+                                                        float confidence = label.getConfidence();
+                                                    }
+                                                }
+                                            }
+                                        })
+                                .addOnFailureListener(
+                                        new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                // Task failed with an exception
+                                                // ...
+                                            }
+                                        });
+                        imageProxy.close();
+                    }
+                });
+
+
             }
         });
-
-        Preview preview = new Preview.Builder().build();
-        CameraSelector cameraSelector = new CameraSelector.Builder()
-                .requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
-        preview.setSurfaceProvider(previewView.getSurfaceProvider());
-        cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector,
-                imageAnalysis, preview);
     }
 }
--- app/src/main/java/com/gruppo6/boatrecognitionapp/MainActivity.java
+++ app/src/main/java/com/gruppo6/boatrecognitionapp/MainActivity.java
@@ -5,9 +5,11 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String[] CAMERA_PERMISSION = new String[]{Manifest.permission.CAMERA};
     private static final int CAMERA_REQUEST_CODE = 10;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
